cefce515d0ffa74f3679ffca7ee69544f9bb6bf1
Iwaneq <iwanskikuba@wp.pl>
2024-03-25 16:42:42 +0100

    Ask for the name at the start

---
+++ ApostlePath/View/AskForNameView.xaml.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Maui.Views;
+
+namespace ApostlePath.View;
+
+public partial class AskForNameView : ContentPage
+{
+	public AskForNameView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ApostlePath/ViewModel/AskForNameViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ApostlePath.ViewModel
+{
+    public class AskForNameViewModel : ObservableObject
+    {
+        public IAsyncRelayCommand SetNameCommand { get; set; }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AskForNameViewModel()
+        {
+            SetNameCommand = new AsyncRelayCommand(SetName);
+        }
+
+        private async Task SetName()
+        {
+            Preferences.Set("Name", Name);
+            await Shell.Current.GoToAsync("..");
+        }
+    }
+}
